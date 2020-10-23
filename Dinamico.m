94c6609c91874862d345811e12f4d15a5788cc1c
christian741 <christian.xd.417@gmail.com>
2020-10-23 00:35:33 -0500

    Arreglo de la funcion

--- Dinamico.m
+++ Dinamico.m
@@ -1,194 +1,203 @@
+% -------------------------
+%               RNABP Dinamica
+% -------------------------
+%-----------------------------------------------------
+%       Definicion de Entradas y Salidas Deseadas
+%------------------------------------------------------
+% X=[0 0 255;
+%    0 255 0;
+%    255 0 0] ; %Entradas
+% YD =[1 0 0;
+%      0 1 0; 
+%      1 0 0] ; %SalidasDeseadas
+% X=[0 0 0; 255 255 255]
+% YD =[1; 0]
+X=[1.5 1.2];
+YD=[0.69];
+% x=[0 0;
+%    0 1;
+%    1 0;
+%    1 1];
+% yd=[0; 1; 1; 1];
+
 %-----------------------------------------------------
 %       Asignacion Manual de entradas neuronas y salidas
 %------------------------------------------------------
-CantidadEntradas = 3;
-CantidadOcultas = 2;
-CantidadSalidas = 2;
+cantidadEntradas = length(X);
+cantidadOcultas = 2;
+cantidadSalidas =length(YD);
+%-----------------------------------------------------
+%                   PESOS DE
+%------------------------------------------------------
+%----------------   OCULTAS    -------------------
+cantidadpesosOcultas = cantidadEntradas*cantidadOcultas;
+pesosOcultas = [];
+pesosOcultas = [0.3 0.7 ;0.5 0.9];
+%for i=1:cantidadEntradas
+    %for j=1:cantidadOcultas
+     %  pesosOcultas(i,j)=randi([1 100],1,1)/100;
+    %end
+%end
 
-
-
-%Alpha = 0.0001;%Coeficiente de aprendizaje
-Alpha = 0.25;
-%------------------------------------------
-%               UMBRALES
-%------------------------------------------
-
-CantidadUmbral =CantidadSalidas+CantidadOcultas;
-UmbralOcultas= [CantidadOcultas]; % en array los umbrales
-UmbralSalidas =[CantidadSalidas];
-%CantidadPesos = (2*CantidadEntradas)+(2*CantidadSalidas);
-
-%
-%           Umbrales Ocultas
-%
-for i = 1:CantidadOcultas
-    UmbralOcultas(i) = 1;
+%----------------   SALIDAS    -------------------
+cantidadpesosSalidas = cantidadOcultas * cantidadSalidas;
+pesosSalidas = [];
+for i=1:cantidadSalidas
+    for j=1:cantidadOcultas
+        %pesosSalidas(i,j)=randi([1 100],1,1)/100;
+    end
 end
-%
-%           Umbrales Salida
-%
-for i = 1:CantidadSalidas
-    UmbralSalidas(i) = 1;
-end
-%------------------------------------------
-%               BIAS
-%------------------------------------------
-
-BiasOcultas= 0.5;
-BiasSalidas = 0.5;
-
-%BiasOcultas= 0;
-%BiasSalidas = 0;
-%------------------------------------------
-%          CAPA DE ENTRADA
-%------------------------------------------
-Entradas = [CantidadEntradas]; %Arreglo de 1*n entradas
-CantidadPesoEntrada = CantidadOcultas*CantidadEntradas;
-PesosEntrada = [CantidadPesoEntrada]
-
-%------------------------------------------
-%          CAPA DE SALIDA
-%------------------------------------------
-Salidas = [CantidadSalidas];%Arreglo de 1*n salidas
-CantidadPesoSalida = CantidadOcultas*CantidadSalidas;
-PesosSalida = [CantidadPesoSalida];
-
-NETsalida = [CantidadSalidas]; %Salida
-ySalida= [CantidadSalidas];%Salidas
-%
-%                   ERROR
-%
-
-DeltaErrorSalida = [CantidadSalidas];%Salidas
-
-
-
-%------------------------------------------
-%                   CAPA OCULTA
-%------------------------------------------
-NETs =[CantidadOcultas];% Ocultas
-yNeuronas = [CantidadOcultas]; %Ocultas
-%
-%                   ERROR
-%
-DeltaErroresOculta = [CantidadPesoEntrada];%Ocultas
-
-
-
+pesosSalidas =[0.1  0.4]
+SalidaDeseada = YD;
 
 %-----------------------------------------------------
-%           Salida deseada
-%----------------------------------------------------
-SalidaDeseada = [CantidadSalidas];
-
-
-
-%----------------------------------------
-%           ERRORES
-%-----------------------------------------
-ErrorPatron = 0;
-ErrorReal =0; %Error de toda la red
-ErrorDefinido = 0.000000001;
-%----------------------------------------
-%           INTEGRACION MANUAL
-%-----------------------------------------
-Entradas(1) = 1;
-Entradas(2) = 4;
-Entradas(3) = 5;
-% Entradas(1) = 0;
-% Entradas(2) = 1;
-%SalidaDeseada (1) =0.58778666;
-% SalidaDeseada (1) =1;
-SalidaDeseada (1) =0.1;
-SalidaDeseada (2) =0.05;
-
-%-------------------------------------------------------
-%               PESOS
-%---------------------------------------------------------
-%
-%     Pesos Randomicos de acuerdo a entradas
-%
-pesosOculta = [0.1,0.2,0.3,0.4,0.5,0.6];
-pesosSalida = [0.7,0.8,0.9,0.1];
-%pesosOculta = [0.1,0.5,-0.7,0.3];
-%pesosSalida = [0.2,0.4];
-for i = 1:CantidadPesoEntrada
-    PesosEntrada(i) = pesosOculta(i);
+%                BIAS
+%------------------------------------------------------
+%----------------   OCULTAS    -------------------
+biasOcultas = [];
+for i=1:cantidadOcultas
+    biasOcultas(i)=0;
 end
-%
-%     Pesos Randomicos de acuerdo a Salidas
-%
-for i = 1:CantidadPesoSalida
-    PesosSalida(i) = pesosSalida(i);
+%----------------   SALIDAS    -------------------
+biasSalidas = [];
+for i=1:cantidadSalidas
+    biasSalidas(i,j)=0;
+end
+biasOcultas = [-0.6 -0.8];
+biasSalidas = [-0.1];
+%-----------------------------------------------------
+%                    NETS DE
+%------------------------------------------------------
+%----------------   OCULTAS    -------------------
+NETOculta = [];
+for i=1:cantidadOcultas
+    NETOculta(i) = 0; 
 end
 
-%**********************************************************************************************
-%                               ENTRENAMIENTO
-%**********************************************************************************************
-par= 2;
-total =0;
-entrenar = false;
-%**************************************************
-%                       NETS OCULTAS
-% Entrada a capa oculta
-% w_1x_1+w_3x_2+w_5x_3+b_1=z_{h_1}
-% 
-% w_2x_1+w_4x_2+w_6x_3+b_1=z_{h_2}
-% 
-% h_1=\sigma(z_{h_1})
-% 
-% h_2=\sigma(z_{h_2})
-%**************************************************
-disp("%-------------------------------");
-disp("%           NetsOcultas         ");
-disp("%-------------------------------");
-for i = 1:CantidadOcultas
-    
-    NETs(i) = 0;
-    total = 0;
-    %Nets de todas las neuronas capa oculta
-    NETs(i)=NetsOcultas(CantidadOcultas,CantidadPesoEntrada,PesosEntrada,CantidadEntradas,Entradas,i,BiasOcultas);
-    %*******************************************
-    %Forward yNeuronas
-    total = total+ NETs(i);
-    NETs(i) = total;
-    disp("NetsOcultas "+NETs(i));
-    yNeuronas(i) = Sigmoideal(NETs(i));
-    disp("YNeuronasOcultas "+yNeuronas(i));
-    
+%----------------   SALIDAS    -------------------
+NETSalida = [];
+for i=1:cantidadSalidas
+    NETSalida(i) = 0; 
 end
-%-------------------------------------------------
-%           NETS de Salida
-%-------------------------------------------------
-disp("%-------------------------------");
-disp("%           NETS de Salida         ");
-disp("%-------------------------------");
-for i = 1:CantidadOcultas
-    NETsalida(i) = 0;
-    total = 0;
-    %Nets de todas las neuronas capa oculta
-    NETsalida(i)=NetsSalida(CantidadSalidas,CantidadPesoSalida,PesosSalida,CantidadOcultas,yNeuronas,i,BiasSalidas)
-    %*******************************************
-    %Forward yNeuronas
-    total = total+ NETsalida(i);
-    NETsalida(i)=total;
-    disp("NetsSalida "+NETsalida(i));
-    ySalida(i) = Sigmoideal(NETsalida(i));
-    disp("YNeuronasSalida "+yNeuronas(i));
-    
-end
-%***********************************************************
-%          CALCULO ERRORES DE DELTA SALIDA BACKPROPAGATION
-%***********************************************************
+%-----------------------------------------------------
+%                    Y con SIGMOIDEAL DE
+%------------------------------------------------------
+%----------------   OCULTAS    -------------------
+yOculta = [];
 
-for i=1:CantidadSalidas;
+%----------------   SALIDAS    -------------------
+ySalida = [];
+
+%-----------------------------------------------------
+%                    DELTAS DE
+%------------------------------------------------------
+%----------------   OCULTAS    -------------------
+deltaOculta = [];
+for i=1:1cantidadOcultas
+    for j=1:1:cantidadEntradas
+        deltaOculta(i,j)=0;
+    end
+end
+%----------------   SALIDAS    -------------------
+deltaSalida = [];
+for i=1:1:cantidadOcultas
+    for j=1:1:cantidadSalidas
+        deltaSalida(i,j) = 0;
+    end
+end
+%-----------------------------------------------------
+%                  DELTA Error DE SALIDA
+%------------------------------------------------------
+DeltaErrorSalida = [];
+for i=1:1cantidadOcultas
+    for j=1:1:cantidadSalidas
+        DeltaErrorSalida(i,j)=0;
+    end
+end
+%-----------------------------------------------------
+%                  Errores
+%------------------------------------------------------
+for i=1:cantidadEntradas
+    errores(i)=0;
+end
+arrayErrorTotal = [];
+%-----------------------------------------------------
+%                  DEFINICION DE VARIABLES
+%------------------------------------------------------
+alfa=0.8;
+errorTotal=100;
+casos=0;
+errorDeseado=0.0000001;
+
+%while errorTotal>errorDeseado
+    for p=1:1:1
         
-    DeltaErrorSalida(i)= (SalidaDeseada(i)-ySalida(i))*ySalida(i)*(1-ySalida(i));
-    disp("DeltaErrorSalida "+DeltaErrorSalida(i));
-end
-%***********************************************************
-%          ERRORES DE DELTA ESTIMADOS
-%***********************************************************
+        for i=1:1:cantidadOcultas %Filas
+            for j=1:1:cantidadEntradas %Columnas
+                NETOculta(p,i)=NETOculta(p,i)+pesosOcultas(j,i)*X(p,j);
+                %disp("****NETOculta(p,j) "+NETOculta(p,i)+ "   i "+i+"  j "+j+"  pesosOcultas "+pesosOcultas(j,i)+"*  X "+X(p,j));
+            end
+        end
+        for j=1:1:cantidadOcultas
+            NETOculta(p,j)=NETOculta(p,j)+biasOcultas(j);
+            yOculta(p,j)=1/(1+exp(-NETOculta(p,j)));
+            %disp(" NETOculta "+NETOculta(p,j));
+            %disp(" yOculta "+ yOculta(p,j));
+        end
+        
+        
+        for j=1:1:cantidadSalidas
+            for i=1:1:cantidadOcultas
+                %disp("Fila "+j+"  Columna "+i);
+                NETSalida(p,j)=NETSalida(p,j)+pesosSalidas(j,i)*yOculta(p,i);
+                %disp("*")
+                %disp("****NETSalida(p,i) "+NETSalida(p,j)+ "   i "+i+"  j "+j+"  pesosSalidas "+pesosSalidas(j,i)+"*  yOculta "+yOculta(p,i));
+                %ySalida(p,i)=1/(1+exp(-neto(p,1)));
+            end
+        end
+        
+        for j=1:1:cantidadSalidas
+            NETSalida(p,j)=NETSalida(p,j)+biasSalidas(j);
+            ySalida(p,j)=1/(1+exp(-NETSalida(p,j)));
+            %disp(" NETSalida "+NETSalida(p,j));
+            %disp(" ySalida "+ ySalida(p,j));
+        end
+        
+        
+         %calculo errores parciales
+        for k=1:1:cantidadSalidas
+            deltaSalida(p,k)=(YD(k)-ySalida(p,k))*ySalida(p,k)*(1-ySalida(p,k));
+        end
+        
+        for j=1:1:cantidadOcultas
+            for k=1:1:cantidadSalidas
+                %disp("Fila "+j+"  Columna "+k);
+                DeltaErrorSalida(p,j)=deltaSalida(p,k)*pesosOcultas(k,j);
+                disp("---DeltaErrorSalida(p,i) "+DeltaErrorSalida(p,j)+ "   j "+j+"  k "+k+"  deltaSalida "+deltaSalida(p,k)+"*  pesosOcultas "+pesosOcultas(k,j));
+            end
+        end
+        for j=1:1:cantidadOcultas
+            for i=1:1:cantidadEntradas
+                %disp("Fila "+i+"  Columna "+j);
+                deltaOculta(p,j)=X(p,i)*(1-X(p,i))*DeltaErrorSalida(p,j);
+                 disp("+++++++++deltaOculta(p,i) "+deltaOculta(p,j)+ "   j "+j+"  k "+k+"  Entradas "+X(p,i)+"*  1-eNTRDA "+X(p,i)+"  DeltaErrorSalida  "+DeltaErrorSalida(p,j));
+            end
+        end
+        
+         % actualizacion de pesos out
+        for j=1:1:2
+            for k=1:1:1
+                wo(k,j)=wo(k,j)+alfa*Do(p,k)*yh(p,j);
+                tk(k)=tk(k)+alfa*Do(p,k);
+            end
+        end
+        
+        
+    end
+    casos=casos+1
+    arrayErrorTotal(casos)=errorTotal;
+    arrayCasos(casos)= casos;
+%end
 
 
 
@@ -207,13 +216,7 @@ end
 
 
 
-%*************************************************************************************
-%*************************************************************************************
-%*************************************************************************************
-%                           FUNCIONES
-%*************************************************************************************
-%*************************************************************************************
-%*************************************************************************************
+
 %-------------------------------
 %           NetsSalida
 %-------------------------------
@@ -230,7 +233,7 @@ for i=indice:CantidadSalidas:CantidadPesoSalida
     iteracionAux = iteracionAux+1;
     
 end
-for j=1:CantidadSalidas%
+for j=1:CantidadOcultas%
     
     acumulador = yNeuronas(j)*vectorAuxiliar(iteracion);
     
@@ -239,12 +242,12 @@ for j=1:CantidadSalidas%
     valor = valor+acumulador;
     disp("Valor es "+valor);
 end
-    valor = valor+BiasSalidas;
+    valor = valor+BiasSalidas(indice);
 end
 %-------------------------------
 %           NetsOcultas
 %-------------------------------
-function valor = NetsOcultas(CantidadOcultas,CantidadPesoEntrada,PesosEntrada,CantidadEntradas,Entradas,indice,BiasOcultas)
+function valor = NetsOcultas(CantidadOcultas,CantidadPesoEntrada,PesosEntrada,CantidadEntradas,Entradas,indice,BiasOcultas,patron)
 
 acumulador= 0;
 valor= 0;
@@ -252,8 +255,8 @@ vectorAuxiliar=[];
 iteracion = 1;
 iteracionAux = 1;
 for i=indice:CantidadOcultas:CantidadPesoEntrada
-    vectorAuxiliar(iteracionAux) =  PesosEntrada(i);
-%     disp("vector auxiliar "+vectorAuxiliar(iteracionAux) + "itearacionAux " +iteracionAux);
+   vectorAuxiliar(iteracionAux) =  PesosEntrada(i);
+   disp("vector auxiliar "+vectorAuxiliar(iteracionAux) + "itearacionAux " +iteracionAux);
     iteracionAux = iteracionAux+1;
     
 end
@@ -266,12 +269,11 @@ for j=1:CantidadEntradas%
     valor = valor+acumulador;
 %     disp("Valor es "+valor);
 end
-valor = valor +BiasOcultas;
+valor = valor +BiasOcultas(indice);
 end
 %-------------------------------
 %       Sigmoideal
 %-------------------------------
 function y = Sigmoideal(net)
-    euler=2.71828;
-    y = 1/(1+(euler^(-1*net)));
+    y = 1/(1+(exp(-net)));
 end
